9a2fad7fb915e4b1a1e5f60dbf091cd4694d43f3
chamodmd <129609995+chamodmd@users.noreply.github.com>
2023-04-03 12:52:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ module.exports = {
     DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
     SUDO: process.env.SUDO || "",
-    LANGUAGE: process.env.LANGUAGE || 'shinhala',
+    LANGUAGE: process.env.LANGUAGE || 'si',
     DEBUG: DEBUG,
     ACR_A: "ff489a0160188cf5f0750eaf486eee74",
     ACR_S: "ytu3AdkCu7fkRVuENhXxs9jsOW4YJtDXimAWMpJp"
